7a77a541dd97f106b3fa2dd91e5d7fd981fdd0ee
Aditi-Adhikari <adhikarii.aditi@gmail.com>
2024-07-08 16:40:13 +0545

    update

---
+++ main.py
@@ -0,0 +1,81 @@
+# print("Hello")
+# print("hello",45678,True)
+
+# name ='ram'
+# age =10
+# address='ktm'
+
+
+# print("my name is ",name)
+# print("age ",age)
+# print("Address ",address)
+
+# print(f"My name is {name} age {age} address {address}") 
+
+
+# name =input("Enter your name: ")
+# print(name)
+
+# +,-,*,/,%, **,//
+
+# a =5
+# b=6
+# print(a+b)
+# print(a-b)
+
+# print(10%3)
+
+# print(4**2)
+
+# print(19//5)
+
+# a =5
+# b=6
+# c=a
+
+# print(a is b)
+# print(a is c)
+# print(a is not b)
+
+# name ='ram'
+
+# print('a' in name)
+# print('a' not in name)
+# print('z' in name)
+
+
+# and, or,not
+# username='admin'
+# password='admin0'
+
+# print(username=='admin' and password=='admin')
+
+# print(username=='admin' or password=='admin')
+
+# name,email,address,phone
+# name=input('Enter your name:')
+# email=input('Enter your address email')
+# address=input('Enter your address')
+# phone=input('Enter your phone')
+
+# print(name)
+# print(email)
+# print(address)
+# print(phone)
+
+# principle=float(input("enter principle:"))
+# Time=float(input("enter time:"))
+# Rate=float(input("enter rate:"))
+# simpleIntrest=principle*Time*Rate/100
+# print(f"simpleIntrest={simpleIntrest}")
+
+Name=input('enter your name:')
+english=int(input('enter your english marks:'))
+nepali=int(input('enter your nepali marks:'))
+science=int(input('enter your science marks:'))
+social=int(input('enter your social marks:'))
+print(Name)
+totalmarks=(english+nepali+science+social)
+totalpercentage=((english+nepali+science+social)/400)*100
+print(f"totalmarks={totalmarks}")
+print(f"totalpercentage={totalpercentage}")
